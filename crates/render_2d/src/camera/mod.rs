8f70608a9e86d580f8b24011b2ea8848be386742
Atlas <gamesworldcraft@gmail.com>
2026-06-12 12:20:15 +0800

    feat: adding cargo config

--- crates/render_2d/src/camera/mod.rs
+++ crates/render_2d/src/camera/mod.rs
@@ -1,14 +1,20 @@
 pub mod demo_camera;
+pub mod demo_camera_follow;
 pub mod example;
 pub mod ui_camera;
 
 use bevy::prelude::*;
 
+pub use demo_camera_follow::{
+    DemoCameraFollow, DemoCameraFollowTarget, demo_camera_follow_system,
+};
 pub use demo_camera::{DemoWorldCamera2d, DemoWorldCamera2dBundle};
 pub use ui_camera::{UI_CAMERA_ORDER, UiCamera, UiCameraConfig};
 
 pub struct Camera2dPlugin;
 
 impl Plugin for Camera2dPlugin {
-    fn build(&self, _app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(PostUpdate, demo_camera_follow_system);
+    }
 }
